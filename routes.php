de6b664d1b2f3a06537d7847f98d7429f246789b
Vulps <ajmcallister5@gmail.com>
2023-10-24 15:06:40 +0100

    Added TorD discord bot terms of Service

--- routes.php
+++ routes.php
@@ -1,8 +1,9 @@
 <?php
 // Define routes and their corresponding template files
 $routes = array(
-    '/' => ['title' => 'Home', 'module' => 'home.php'],
-    '/vortex-evader' => ['title' => 'Vortex Evader', 'module' => 'vortex-evader.php'],
-    '/track-my-miner' => ['title' => 'TrackMyMiner', 'module' => 'track-my-miner.php'],
-    '/steam-bot' => ['title' => 'Steam Bot (TBD)', 'module' => 'steam-bot.php'],
+    '/' => ['title' => 'Home', 'module' => 'home.php', 'hidden' => false],
+    '/vortex-evader' => ['title' => 'Vortex Evader', 'module' => 'vortex-evader.php', 'hidden' => false],
+    '/track-my-miner' => ['title' => 'TrackMyMiner', 'module' => 'track-my-miner.php', 'hidden' => false],
+    '/steam-bot' => ['title' => 'Steam Bot (TBD)', 'module' => 'steam-bot.php', 'hidden' => false],
+	'/tord/terms' => ['title' => 'Truth Or Dare 18+ Terms of Service', 'module' => 'tord-terms.php', 'hidden' => false],
 );
