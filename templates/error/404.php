07c464cdf4e38ab84f37a246446a833aa7e977d4
Vulps <ajmcallister5@gmail.com>
2025-08-05 00:05:36 +0100

    Add Timey Zoney

--- templates/error/404.php
+++
@@ -1,6 +0,0 @@
-<main>
-    <section>
-        <h2>404 Not Found</h2>
-        <p>Sorry, the page you are looking for could not be found.</p>
-    </section>
-</main>
--- templates/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Vulps. All rights reserved.</p>
-</footer>
-</body>
-</html>
